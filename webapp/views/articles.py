0c51a87120d962620cd138126a2ceb733f10b417
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-02-24 15:06:20 +0600

    Изменения в файле articles.py в связи с добавлением формы

--- webapp/views/articles.py
+++ webapp/views/articles.py
@@ -1,37 +1,68 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect, get_object_or_404
-from webapp.models import Task
+from webapp.forms import TaskForm
+from webapp.models import Task, StatusChoice
 
 
 def add_view(request: WSGIRequest):
     if request.method == "GET":
-        return render(request, 'task_create.html')
-    task_data = {
-        'title': request.POST.get('title'),
-        'description': request.POST.get('description'),
-        'details': request.POST.get('details'),
-        'deadline': request.POST.get('deadline'),
-        'status': request.POST.get('status'),
-    }
-    task = Task.objects.create(**task_data)
-    return redirect('task_detail', pk=task.pk)
+        form = TaskForm()
+        return render(request, 'task_create.html',
+                      context={
+                          'choices': StatusChoice.choices,
+                          'form': form
+                      })
+    form = TaskForm(data=request.POST)
+    if not form.is_valid():
+        return render(request, 'task_create.html',
+                      context={
+                          'choices': StatusChoice.choices,
+                          'form': form
+                      })
+    else:
+        task = Task.objects.create(**form.cleaned_data)
+        return redirect('task_detail', pk=task.pk)
 
 
-def update_view(request: WSGIRequest, pk):
+def update_view(request, pk):
+    errors = {}
     task = get_object_or_404(Task, pk=pk)
-    if request.method == "GET":
-        deadline = str(task.deadline)
-        return render(request, 'task_update.html', context={'task': task, 'deadline': deadline, 'status': task.status})
-    deadline = None
-    if request.POST.get('deadline'):
-        deadline = request.POST.get('deadline')
-    task.title = request.POST.get('title'),
-    task.description = request.POST.get('description'),
-    task.details = request.POST.get('details'),
-    task.deadline = deadline
-    task.status = request.POST.get('status'),
-    task.save()
-    return redirect('task_detail', pk=task.pk)
+
+    if request.method == 'GET':
+        form = TaskForm(initial={
+            'title': task.title,
+            'description': task.description,
+            'details': task.details,
+            'deadline': task.deadline,
+            'status': task.status
+        })
+        return render(request, 'task_update.html',
+                      context={
+                          'task': task,
+                          'choices': StatusChoice.choices,
+                          'form': form
+                      })
+    elif request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task.title = form.cleaned_data['title']
+            task.details = form.cleaned_data['details']
+            task.description = form.cleaned_data['description']
+            task.deadline = form.cleaned_data['deadline']
+            task.status = form.cleaned_data['status']
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+
+        else:
+            if not request.POST.get('title'):
+                errors['title'] = 'Данное поле обязательно к заполнению'
+            return render(request, 'task_update.html',
+                          context={
+                              'task': task,
+                              'choices': StatusChoice.choices,
+                              'errors': errors,
+                              'form': form
+                          })
 
 
 def detail_view(request, pk):
@@ -42,6 +73,11 @@ def detail_view(request, pk):
 
 
 def delete_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_confirm_delete.html', context={'task': task})
+
+
+def confirm_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.delete()
     return redirect('index')
